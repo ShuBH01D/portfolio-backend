1e458e7047911e66f003882c4726f00a771ca653
Shubham Dighe <shubhamdighe161909@gmail.com>
2026-04-28 15:22:30 +0530

    update Db

--- config/db.js
+++ config/db.js
@@ -1,19 +1,26 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Shubh@123",
-  database: "portfolio_db"
-});
+// 🔥 Only connect DB in LOCAL (not in production / Render)
+let connection = null;
 
-connection.connect((err) => {
-  if (err) {
-    console.error("DB connection failed ❌");
-    console.log(err);
-  } else {
-    console.log("MySQL Connected ✅");
-  }
-});
+if (process.env.NODE_ENV !== "production") {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Shubh@123",
+    database: "portfolio_db"
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("DB connection failed ❌");
+      console.log(err);
+    } else {
+      console.log("MySQL Connected ✅");
+    }
+  });
+} else {
+  console.log("⚠️ DB disabled in production (Render)");
+}
 
 module.exports = connection;
